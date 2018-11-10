7c912e37fe62b8852928d42240bb71a8c51084c3
Rohan <c1ph3r9@gmail.com>
2018-11-10 01:14:08 -0600

    add post /todos route

--- TODO-REST-API/server/tests/server.test.js
+++ TODO-REST-API/server/tests/server.test.js
@@ -6,7 +6,7 @@ const {app}=require('./../server')
 const {Todo}=require('./../models/todo')
 
 beforeEach((done)=>{
-    Todo.remove({})
+    Todo.remove({}).then(()=> done())
 })
 
 describe('POST /todos',()=>{
@@ -30,6 +30,21 @@ describe('POST /todos',()=>{
             Todo.find().then((todos)=>{
                 expect(todos.length).toBe(1)
                 expect(todos[0].text).toBe(text)
+                done()
+            }).catch((e)=> done(e))
+        })
+    })
+    it('should not create todo with invalid data', (done)=>{
+        request(app)
+        .post('/todos')
+        .send({})
+        .expect(400)
+        .end((err,res)=>{
+            if(err){
+                return done(Err)
+            }
+            Todo.find().then((todos)=>{
+                expect(todos.length).toBe(0)
                 done()
             }).catch((e)=> done(e))
         })
